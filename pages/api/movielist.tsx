40707fecd4e33ed9e7015bb0a41bb65f3a55fe24
michielpierik <michiel@skrepr.com>
2022-02-17 09:23:21 +0100

    Yup.

--- pages/api/movielist.tsx
+++ pages/api/movielist.tsx
@@ -7,13 +7,13 @@ const handle = async (
 ) => {
 
     const page = req.query.page ?? 1;
+    const tmdbKey = process.env.TMDB_KEY;
     try {
         const result = await axios.get(`https://api.themoviedb.org/4/discover/movie?sort_by=popularity.desc&page=${page}`, {
-            headers: { Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlYjdiMTk4NTEyNGIzYThmZDBkMTI3NzBlOTgxMTAwNCIsInN1YiI6IjYyMGNiOWYxZTI3MjYwMDAxYWI5YTU3MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Pak8HtiLpfAvf07u4Uwe4igbZjX7Nw_efUXne9cOQtQ` },
+            headers: { Authorization: `Bearer ${tmdbKey}` },
         });
         res.status(200).json(result.data);
     } catch(error) {
-        console.log(error)
         res.status(500).json({message: 'nope'});
     }
 }
